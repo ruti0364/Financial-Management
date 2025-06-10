be4c09658034801a29889cc06b24b77285e0dafc
sarit malkiel <sarit48694@gmail.com>
2025-06-10 10:38:54 +0300

    login

--- finance-server/routes/login.js
+++ finance-server/routes/login.js
@@ -1,40 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');  
- 
+const bcrypt = require('bcrypt');
+
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
 
-  
   if (!email || !password) {
     return res.status(400).json({ error: 'Email and password are required' });
   }
 
   try {
     const user = await User.findOne({ email });
-
     if (!user) {
-      
-      return res.status(401).json({ error: 'Invalid password or Email' });
+      return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-     
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid password or Email' });
-
+      return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-     
-    return res.status(200).json({
-      message: 'User registered successfully',
-      userId: newUser._id
+    res.status(200).json({
+      message: 'Login successful',
+      userId: user._id,
     });
-
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
